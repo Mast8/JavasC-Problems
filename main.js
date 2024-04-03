3f7d2e42e16a42bb0bb24fa35b3032575b13e5ee
mast8 <ronald.miranda.rodriguez@gmail.com>
2024-04-03 18:07:38 -0400

    maze 1.2

--- main.js
+++ main.js
@@ -97,7 +97,7 @@ console.log( printMaze( mazeTest ) );
   [1,0,0,0,1,1,1,'e']];
 
 
- function findpath(x,y){
+/*  function findpath(x,y){
   // illegal move check
   if (x < 0 || x > (map[0].length -1) || y < 0 || y > (map.length - 1)) return false; //if it is outside of map
     //if (map[y][x] == 0) return false; //it is not open
@@ -126,6 +126,31 @@ console.log( printMaze( mazeTest ) );
       return true;              
 
   return false;
+};  */
+
+
+function findpath(x,y){
+  // illegal move check
+  if(valid(map,x,y) ){
+    if (map[y][x] == 0) return false
+    // end move check
+    if (map[x][y] === 'e'){
+      console.log('Reached goal at: ' + x + ':' + y);
+    
+      return true; // if it is the goal (exit point)
+    }
+    if(map[y][x] == 9 || map[y][x] == 8)
+      return false;
+
+  console.log('Im here at: ' + x + ':' + y);
+  map[y][x]=9; //here marking x,y position as part of solution path outlined by "9"
+
+  findpath(x+1,y)
+  findpath(x,y+1);
+  findpath(x,y-1);
+  findpath(x-1,y);
+  }
+  return false ;
 }; 
 
 
@@ -133,9 +158,9 @@ console.log( printMaze( mazeTest ) );
 
 
 
-function valid(x,y) {
-  if(x < 0 || x > (map[0].length -1) || y < 0 || y > (map.length - 1)) return false;
+function valid(map,x,y) {
+  if(x >= 0 && x < (map.length) && y >= 0 && y < (map[0].length)) return true;
 }
-//console.log( printMaze( map ) );
+
 console.log(map)
 findpath(0, 0);
